c1bec9e9cf1690af19e4858d969dd14852db0c63
Nishant Modi <live.nm@gmail.com>
2024-04-18 23:04:22 +0530

    update flow

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -17,7 +17,7 @@ export const app = new Frog({
   assetsPath: "/",
   basePath: "/api",
   hub: neynarHub({ apiKey: `${process.env.NEYNAR_API_KEY}` }),
-  //browserLocation: ADD_URL,
+  browserLocation: ADD_URL,
 }).use(
   neynar({
     apiKey: `${process.env.NEYNAR_API_KEY}`,
@@ -26,7 +26,7 @@ export const app = new Frog({
 );
 
 // Cast action GET handler
-app.hono.get("/rank-action", async (c) => {
+app.get("/rank-action", async (c) => {
   return c.json({
     name: "Check Rank",
     icon: "thumbsup",
