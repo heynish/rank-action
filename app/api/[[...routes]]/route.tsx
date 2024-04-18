221ef75f1734670d7e7aaf566367327950f49782
Nishant Modi <live.nm@gmail.com>
2024-04-18 20:07:13 +0530

    fix1

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource frog/jsx */
 
-import { Button, Frog, TextInput } from 'frog'
+import { Button, Frog } from 'frog'
 import { devtools } from 'frog/dev'
 import { neynar as hub_neynar } from 'frog/hubs'
 import { neynar } from "frog/middlewares";
@@ -10,17 +10,8 @@ import { serveStatic } from 'frog/serve-static'
 const app = new Frog({
   assetsPath: "/",
   basePath: "/api",
-  initialState: {
-    saved: false,
-  },
   hub: hub_neynar({ apiKey: `${process.env.NEYNAR_API_KEY}` }),
 });
-/* .use(
-  neynar({
-    apiKey: `${process.env.NEYNAR_API_KEY}`,
-    features: ["interactor", "cast"],
-  })
-); */
 
 // Uncomment to use Edge Runtime
 // export const runtime = 'edge'
@@ -39,37 +30,44 @@ app.frame('/', (c) => {
         name="Get Global Rank"
         icon="log"
       >
-        Add
+        Add Action
       </Button.AddCastAction>,
     ],
   })
 })
 
 app.castAction("/rank-action", neynarMiddleware, async (c) => {
-  if (c.verified) {
-    //const fid = c.actionData.fid;
-    const username = c.var.interactor?.displayName;
 
-    const response = await fetch('https://graph.cast.k3l.io/scores/global/following/handles', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify([username])
-    })
+  console.log(
+    `Cast Action to ${JSON.stringify(c.actionData.castId)} from ${c.actionData.fid
+    }`,
+  )
+  return c.res({ message: 'Action Succeeded' })
 
-    if (!response.ok) {
-      return c.res({ message: 'Failed to call Openrank API' });
-    }
-
-    const data = await response.json()
-    console.log('data', data);
-
-    return c.res({ message: data });
-  } else {
-    return c.res({ message: "Unauthorized" });
-  }
+  /*  if (c.verified) {
+     //const fid = c.actionData.fid;
+     const username = c.var.interactor?.displayName;
+ 
+     const response = await fetch('https://graph.cast.k3l.io/scores/global/following/handles', {
+       method: 'POST',
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json'
+       },
+       body: JSON.stringify([username])
+     })
+ 
+     if (!response.ok) {
+       return c.res({ message: 'Failed to call Openrank API' });
+     }
+ 
+     const data = await response.json()
+     console.log('data', data);
+ 
+     return c.res({ message: data });
+   } else {
+     return c.res({ message: "Unauthorized" });
+   } */
 });
 
 devtools(app, { serveStatic })
